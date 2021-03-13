8a4e7a0ae2abb7b07349fe01e2f1a4f9adb8dd97
nitishareddy19 <nitisha.ambati@gmail.com>
2021-03-13 13:31:02 +0530

    second commit

---
+++ StaticBlock.java
@@ -0,0 +1,15 @@
+public class StaticBlock{
+	//static variable
+	static int id = 30;
+
+	//constructor
+	StaticBlock(){
+		System.out.println("constructor");
+	}
+
+	//static block
+	static{
+		System.out.println("id = " + id);
+		System.out.println("static block");
+	}
+}
