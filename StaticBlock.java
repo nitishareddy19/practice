a49ccac4ccf93ce598b70058ae60b03fbfeda101
GurramSrikanthReddy <gurramsrikanthreddy12@gmail.com>
2021-03-21 15:37:18 +0530

    modified5

--- StaticBlock.java
+++ StaticBlock.java
@@ -10,9 +10,11 @@ public class StaticBlock{
 	//static block
 	static{
 		System.out.println("id = " + id);
+		System.out.println("id = " + id);
+		System.out.println("id = " + id);
+		System.out.println("id = " + id);
+		System.out.println("id = " + id);
 		System.out.println("static block1");
-		System.out.println("static block2");
-		System.out.println("static block3");
-		System.out.println("static block4");
+
 	}
 }
