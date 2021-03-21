4c81e3836b4ff50c31dbd8b3d174588c69905cfc
Vishnu123i <vishnume06@gmail.com>
2021-03-21 15:58:19 +0530

    modifeid5

--- StaticBlock.java
+++ StaticBlock.java
@@ -12,32 +12,7 @@ public class StaticBlock{
 	static{
 		System.out.println("id = " + id);
 
-<<<<<<< HEAD
-=======
-		System.out.println("id = " + id);
-		System.out.println("id = " + id);
-		System.out.println("id = " + id);
-		System.out.println("id = " + id);
-		System.out.println("static block1");
 
-
-
-		System.out.println("static block5");
-                System.out.println("static block6");
-		System.out.println("static block7");
-		System.out.println("static block8");
-
-		
-                System.out.println("static block1");
-                System.out.println("static block2");
-                System.out.println("static block3");
-
-		System.out.println("static block1");
-		System.out.println("static block2");
-		System.out.println("static block3");
-		System.out.println("static block4");
-               System.out.println("name= " + name);
->>>>>>> ad7a0fa313688a806bed545ddcd72c81e125f225
 		
 
 
